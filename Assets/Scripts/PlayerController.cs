6922758dcdf80ac6a249491b61559d073f5dfa9c
Jigurd <sigurd.stapnes@gmail.dom>
2020-01-27 16:20:18 +0100

    Added High Level NavMesh components to project (NavMeshSurfaces etc) Added PlayerController script

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PlayerController : MonoBehaviour
+{
+
+    private Animator _animator;
+    private NavMeshAgent _navMeshAgent;
+    private bool _running;
+
+    private void Awake()
+    {
+        _animator = GetComponent<Animator>();
+        _navMeshAgent = GetComponent<NavMeshAgent>();
+    }
+
+    // If mouse button is down, set that as new destination for the player.
+    void Update()
+    {
+        if(Input.GetMouseButton(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            RaycastHit hit;
+
+            if(Physics.Raycast(ray, out hit, 100))
+            {
+                _navMeshAgent.destination = hit.point;
+            }
+        }
+
+        if (_navMeshAgent.remainingDistance <= _navMeshAgent.stoppingDistance)
+        {
+            _running = false;
+        } else
+        {
+            _running = true;
+        }
+
+        _animator.SetBool("running", _running);
+    }
+}
