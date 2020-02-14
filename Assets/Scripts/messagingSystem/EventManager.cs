76c60ce3ce68aee1dce3c26c7023854d031efc91
Stig Thomas Gulbrandsen <13stigul@gmail.com>
2020-02-14 12:34:31 +0100

    Simple event messaging system added

---
+++ Assets/Scripts/messagingSystem/EventManager.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class EventManager : MonoBehaviour
+{
+    private Dictionary<string, UnityEvent> _eventDictionary;
+
+    private static EventManager _eventManager;
+
+
+
+    //Get the EventManager if there's one present in the scene
+    public static EventManager instance 
+    {
+        get 
+        {
+            if(!_eventManager)
+            {
+                _eventManager = FindObjectOfType(typeof(EventManager)) as EventManager;
+                //If there's no eventmanager send an error, else intialize the dictionary.
+                if(!_eventManager)
+                {
+                    Debug.LogError("put EventManager on a GameObject in your scene first.");
+                }
+                else
+                {
+                    _eventManager.Init();
+                }
+            }
+            return _eventManager;
+        }
+    }
+
+    //Initialize the dictionary for the eventManager
+    void Init()
+    {
+        if(_eventDictionary == null)
+        {
+            _eventDictionary = new Dictionary<string, UnityEvent>();
+        }
+    }
+
+    //Make the listener start listening
+    public static void StartListening(string eventName, UnityAction listener)
+    {
+        UnityEvent thisEvent = null;
+        //If there's an event like this in the dictionary, add the listener
+        if(instance._eventDictionary.TryGetValue(eventName, out thisEvent))
+        {
+            thisEvent.AddListener(listener);
+        }
+        //Else add the event to the dictionary.
+        else
+        {
+            thisEvent = new UnityEvent();
+            thisEvent.AddListener(listener);
+            instance._eventDictionary.Add(eventName, thisEvent);
+        }
+    }
+    //stop listening to the event
+    public static void StopListening(string eventName, UnityAction listener)
+    {
+        if (_eventManager == null) 
+            return;
+        UnityEvent thisEvent = null;
+        //if there's an event like this in the dictionary, remove the listener
+        if(instance._eventDictionary.TryGetValue(eventName, out thisEvent))
+        {
+            thisEvent.RemoveListener(listener);
+        }
+    }
+    //Invoke the damn event
+    public static void TriggerEvent(string eventName)
+    {
+        UnityEvent thisEvent = null;
+        if(instance._eventDictionary.TryGetValue(eventName, out thisEvent))
+        {
+            thisEvent.Invoke();
+        }
+    }
+}
---
+++ Assets/Scripts/messagingSystem/EventTriggerTest.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EventTriggerTest : MonoBehaviour
+{
+ 
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("q"))
+        {
+            EventManager.TriggerEvent("test");
+        }
+        if (Input.GetKeyDown("o"))
+        {
+            EventManager.TriggerEvent("Spawn");
+        }
+        if (Input.GetKeyDown("p"))
+        {
+            EventManager.TriggerEvent("Destroy");
+        }
+        if (Input.GetKeyDown("x"))
+        {
+            EventManager.TriggerEvent("Junk"));
+        }
+    }
+}
