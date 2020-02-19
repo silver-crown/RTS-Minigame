61864b0c010340c8920468042199080321c1ff73
vsclause <vsclause@stud.ntnu.no>
2020-02-19 15:36:18 +0100

    Created a laser that can be shot from fps view, will modify it to work on combat drone

---
+++ Assets/Scripts/DroneScripts/CombatDrone.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CombatDrone : Drone
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
