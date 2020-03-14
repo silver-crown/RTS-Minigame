6fd9f084dbb3f270a3282424988f1fcd3297efa8
Stig Thomas Gulbrandsen <13stigul@gmail.com>
2020-03-14 13:29:03 +0100

    Constructed empty behaviour trees for bumrush and flanking

---
+++ Assets/bbBT/Scripts/Behaviours/Concrete/BbbtGroupAttackEnemy.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using RTS;
+namespace Bbbt
+{
+    /// <summary>
+    /// Decorator node for checking if there's enough forces to risk attacking an enemy
+    /// </summary>
+    [CreateAssetMenu(fileName = "Group Attack Enemy", menuName = "bbBT/Behaviour/Leaf/Group Attack Enemy", order = 0)]
+    public class BbbtGroupAttackEnemy : BbbtLeafBehaviour
+    {
+
+        public override string SaveDataType { get; } = "BbbtGroupAttackEnemy";
+
+        protected override void OnInitialize(GameObject gameObject)
+        {   
+
+        }
+
+        protected override void OnTerminate(GameObject gameObject, BbbtBehaviourStatus status)
+        {
+        }
+
+        protected override BbbtBehaviourStatus UpdateBehaviour(GameObject gameObject)
+        {
+            return BbbtBehaviourStatus.Failure;
+        }
+    }
+}
+
