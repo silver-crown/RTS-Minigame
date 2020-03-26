d7868d9cb0b2031f2ad55076b5930bd89dd67886
Victor <vsclause@stud.ntnu.no>
2020-03-26 10:41:43 +0100

    Removed combat scene and networking scene

---
+++ Assets/Scripts/Networking/NetworkManager/NetworkManagerRTS.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using Mirror;
+
+namespace RTS.Networking
+{
+    /// <summary>
+    /// Custom networking manager
+    /// </summary>
+    public class NetworkManagerRTS : NetworkManager
+    {
+        /// <summary>
+        /// The minimum amout of players needed before the 
+        /// </summary>
+        [SerializeField] private int _minPlayers;
+
+        /// <summary>
+        /// The scene attribute allows us to drag in secene refernce in the editor
+        /// </summary>
+        [Scene] [SerializeField] private string menuScene = "";
+
+
+    }
+}
