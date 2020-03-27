844647c7e97261d05f12a4759898eaa6553a23c1
Victor <vsclause@stud.ntnu.no>
2020-03-27 14:56:31 +0100

    Network lobby sorta works now!

---
+++ Assets/Scripts/Networking/RTSNetworkRoomPlayer.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using Mirror;
+
+namespace RTS.Networking
+{
+    /// <summary>
+    /// This class will be used to represent the player in the game lobby
+    /// </summary>
+    [AddComponentMenu("")]
+    public class RTSNetworkRoomPlayer : NetworkRoomPlayer
+    {
+        public override void OnStartClient()
+        {
+            Debug.Log("Client Started");
+
+            base.OnStartClient();
+        }
+
+        public override void OnClientEnterRoom()
+        {
+            Debug.Log("Client Enterd Room");
+        }
+
+        public override void OnClientExitRoom()
+        {
+            Debug.Log("Client Exited Room");
+        }
+
+    }
+
+}
